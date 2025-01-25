007c01bd155b35d5ecb1db5ec4cfb0cea7018479
Vignesh-604 <vig6604@gmail.com>
2025-01-26 02:59:38 +0530

    Added Sticky scroll

--- 1-music-app/src/app/page.tsx
+++ 1-music-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
 	return (
 		<div className="bg-black/[0.9] min-h-screen bg-grid-white/[0.02]">
 			<HeroSection />
 			<FeaturedCourses />
+			<WhyChooseUs />
 		</div>
 	);
 }
