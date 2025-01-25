cf98256112659b1ea30e714369db5a77aee53f72
Vignesh-604 <vig6604@gmail.com>
2025-01-26 01:25:50 +0530

    Added Hero section. Spotlight added

--- 1-music-app/src/app/page.tsx
+++ 1-music-app/src/app/page.tsx
@@ -1,5 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
-  return (
-    <h1 className="bg-black/[0.9] min-h-screen flex  justify-center bg-grid-white/[0.02]">Hello NextJS!!</h1>
-  );
+	return (
+		<h1 className="bg-black/[0.9] min-h-screen flex  justify-center bg-grid-white/[0.02]">
+			<HeroSection />
+		</h1>
+	);
 }
