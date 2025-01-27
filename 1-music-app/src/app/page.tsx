3d1a9fcc169451cb3bfa483b6ae09343ad5b3273
Vignesh-604 <vig6604@gmail.com>
2025-01-28 01:51:37 +0530

    Added Card Hover Effect, Animated Tooltip and Wavy Background

--- 1-music-app/src/app/page.tsx
+++ 1-music-app/src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "../components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
 	return (
@@ -10,6 +12,8 @@ export default function Home() {
 			<FeaturedCourses />
 			<WhyChooseUs />
 			<TestimonialCards/>
+			<UpcomingWebinars/>
+			{/* <Instructors/> */}
 		</div>
 	);
 }
